0a6f4bc4914d180b7ff72bc57b25952898a08b9b
Sean Murphy <sean@iamseanmurphy.com>
2025-04-18 08:18:40 +0000

    1.4

--- wider-admin-menu/wider-admin-menu.php
+++ wider-admin-menu/wider-admin-menu.php
@@ -10,7 +10,7 @@
  * License:                 GPLv3 or later
  * License URI:             http://www.gnu.org/licenses/gpl-3.0.html
  * Requires PHP:            5.6
- * Tested up to:            6.7
+ * Tested up to:            6.8
  *
  * Copyright 2014-2019      Chris Dillon        chris@strongwp.com
  * Copyright 2019           MachoThemes         office@machothemes.com
